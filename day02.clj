a83324ad83eeb5ae65f8df39af1388241d18b07e
Ashley Claymore <acutmore@users.noreply.github.com>
2018-12-02 18:28:33 +0000

    Day02 partB done

--- day02.clj
+++ day02.clj
@@ -20,3 +20,31 @@
   (delay (let [char-counts (map #(repeat-counts (val-counts %)) codes)
                sum-counts (val-counts (flatten char-counts))]
            (* (get sum-counts 2) (get sum-counts 3)))))
+
+(defn different-by-one?
+  [a b] (= 1 (count (filter identity (map not= a b)))))
+
+(defn find-adjacent
+  ([] nil)
+  ([a] nil)
+  ([a b & rest] (if (different-by-one? a b)
+                  [a b]
+                  nil)))
+
+(defn process
+  [list]
+  (loop [rem list]
+    (let [v (apply find-adjacent rem)]
+      (if (nil? v)
+        (recur (rest rem))
+        v))))
+
+(defn keep-same
+  [a b] (if (= a b) a nil))
+
+(def answer-part-b
+  (delay
+   (let [[code-a code-b] (process (sort codes))]
+     (apply str
+            (filter char?
+                    (map keep-same code-a code-b))))))

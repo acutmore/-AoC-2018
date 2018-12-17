7c50dc5dce6cfb1bb1209b6708b6dab4cb2b0709
Ashley Claymore <acutmore@users.noreply.github.com>
2018-12-17 21:01:27 +0000

    Day16 partA done

--- day16.clj
+++ day16.clj
@@ -3,7 +3,7 @@
 
 (def raw-input (slurp "./input/day16a.txt"))
 
-(def steps (str/split-lines raw-input))
+(def lines (str/split-lines raw-input))
 
 (def four-digits #"(\d+),? (\d+),? (\d+),? (\d+)")
 
@@ -15,67 +15,43 @@
   [b] (if b 1 0))
 
 (defn cpu-call
-  [f regs [_ ra rb rc :as operands]]
-  (f regs operands (map get [regs regs regs] [ra rb rc])))
+  [f regs [_ va vb rc]]
+  (let [[ra rb] (map get [regs regs] [va vb])
+        result (f [ra rb] [va vb])]
+    (assoc regs rc result)))
 
 (def cpu
-  {:addr (fn [[r0 r1 r2 r3 :as regs] [_ va vb vc] [ra rb rc]]
-           (assoc (vec regs) rc (+ ra rb)))
-   :addi (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (+ ra vb)))
-   :mulr (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (* ra rb)))
-   :muli (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (* ra vb)))
-   :banr (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bit-and ra rb)))
-   :bani (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bit-and ra vb)))
-   :borr (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bit-or ra rb)))
-   :bori (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bit-or ra vb)))
-   :setr (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc ra))
-   :seti (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc va))
-   :gtir (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bool-to-num (> va rb))))
-   :gtri (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bool-to-num (> ra vb))))
-   :gtrr (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bool-to-num (> ra rb))))
-   :eqir (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bool-to-num (= va rb))))
-   :eqri (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bool-to-num (= ra vb))))
-   :eqrr (fn [[r0 r1 r2 r3 :as regs] [_ ra rb rc] [va vb vc]]
-           (assoc (vec regs) rc (bool-to-num (= ra rb))))})
+  {:addr (fn [[ra rb] [va vb]] (+ ra rb))
+   :addi (fn [[ra rb] [va vb]] (+ ra vb))
+   :mulr (fn [[ra rb] [va vb]] (* ra rb))
+   :muli (fn [[ra rb] [va vb]] (* ra vb))
+   :banr (fn [[ra rb] [va vb]] (bit-and ra rb))
+   :bani (fn [[ra rb] [va vb]] (bit-and ra vb))
+   :borr (fn [[ra rb] [va vb]] (bit-or ra rb))
+   :bori (fn [[ra rb] [va vb]] (bit-or ra vb))
+   :setr (fn [[ra rb] [va vb]] ra)
+   :seti (fn [[ra rb] [va vb]] va)
+   :gtir (fn [[ra rb] [va vb]] (bool-to-num (> va rb)))
+   :gtri (fn [[ra rb] [va vb]] (bool-to-num (> ra vb)))
+   :gtrr (fn [[ra rb] [va vb]] (bool-to-num (> ra rb)))
+   :eqir (fn [[ra rb] [va vb]] (bool-to-num (= va rb)))
+   :eqri (fn [[ra rb] [va vb]] (bool-to-num (= ra vb)))
+   :eqrr (fn [[ra rb] [va vb]] (bool-to-num (= ra rb)))})
 
 (defn sample-matches
   [[before instruction after]]
   (as-> cpu $
-    (map second $)
-    (map #(cpu-call % before instruction) $)
-    (filter #(= after %) $)))
+    (map #(vec [(first %) (cpu-call (second %) before instruction)]) $)
+    (filter #(= after (second %)) $)))
 
-(defn answer-part-a
-  []
-  nil)
-
-(let [parse #(map parse-four-digits %)
-      befores (parse (take-nth 4 steps))
-      instructions (parse (take-nth 4 (rest steps)))
-      afters (parse (take-nth 4 (nthrest steps 2)))
-      samples (map list befores instructions afters)]
-  (as-> samples $
-    (map sample-matches $)
-    (map count $)
-    (filter #(> % 2) $)
-    (count $)))
-
-; 537 - too low
-
-; 605
-
-; 674 - too high
+(def answer-part-a
+  (delay (let [parse #(map parse-four-digits %)
+               befores (parse (take-nth 4 lines))
+               instructions (parse (take-nth 4 (rest lines)))
+               afters (parse (take-nth 4 (nthrest lines 2)))
+               samples (map list befores instructions afters)]
+           (->> samples
+                (map sample-matches)
+                (map count)
+                (filter #(> % 2))
+                (count)))))

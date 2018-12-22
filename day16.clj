a88380146255958af956893c21339c35fc178cba
Ashley Claymore <acutmore@users.noreply.github.com>
2018-12-22 22:11:19 +0000

    Day16 partB done

--- day16.clj
+++ day16.clj
@@ -2,8 +2,10 @@
   (:require [clojure.string :as str]))
 
 (def raw-input (slurp "./input/day16a.txt"))
+(def raw-input-b (slurp "./input/day16b.txt"))
 
 (def lines (str/split-lines raw-input))
+(def lines-b (str/split-lines raw-input-b))
 
 (def four-digits #"(\d+),? (\d+),? (\d+),? (\d+)")
 
@@ -40,9 +42,12 @@
 
 (defn sample-matches
   [[before instruction after]]
-  (as-> cpu $
-    (map #(vec [(first %) (cpu-call (second %) before instruction)]) $)
-    (filter #(= after (second %)) $)))
+  (->> cpu
+       (map (fn [[name operation]]
+              {:opname name
+               :result (cpu-call operation before instruction)
+               :opcode (first instruction)}))
+       (filter #(= after (:result %)))))
 
 (def answer-part-a
   (delay (let [parse #(map parse-four-digits %)
@@ -55,3 +60,88 @@
                 (map count)
                 (filter #(> % 2))
                 (count)))))
+
+(def opcodes {0 (cpu :mulr)
+              7 (cpu :bani)
+              1 (cpu :eqri)
+              4 (cpu :gtrr)
+              15 (cpu :bori)
+              13 (cpu :gtri)
+              6 (cpu :borr)
+              3 (cpu :eqrr)
+              12 (cpu :addi)
+              2  (cpu :setr)
+              11 (cpu :gtir)
+              9 (cpu :banr)
+              5 (cpu :muli)
+              14 (cpu :seti)
+              10 (cpu :eqir)
+              8 (cpu :addr)})
+
+(def answer-part-b
+  (delay ( (->> lines-b
+                (map parse-four-digits)
+                (reduce (fn [regs pc]
+                          (let [opcode (first pc)
+                                operation (opcodes opcode)]
+                            (cpu-call operation regs pc)))
+                        (vec [0 0 0 0]))))))
+
+(defn set-set
+  [prev v]
+  (if (set? prev)
+    (conj prev v)
+    #{v}))
+
+(comment
+  ;--- Calculating opcode map for part -b
+  (let [parseFn #(map parse-four-digits %)
+        befores (parseFn (take-nth 4 lines))
+        instructions (parseFn (take-nth 4 (rest lines)))
+        afters (parseFn (take-nth 4 (nthrest lines 2)))
+        samples (map list befores instructions afters)]
+    (->> samples
+         (map sample-matches)
+         (flatten)
+         (reduce (fn [map {:keys [opname opcode]}] (update map opcode set-set opname)) {})
+         (clojure.pprint/pprint)))
+
+  ; -- Above gives this map
+  {0 #{:mulr}
+   7 #{:bori :bani :gtir :eqir :borr :addr :gtrr :setr :eqri :muli}
+   1 #{:eqir :borr :addr :seti :eqri}
+   4 #{:seti :gtrr :gtri}
+   15 #{:bori :borr}
+   13 #{:eqrr :seti :gtri}
+   6 #{:borr :mulr}
+   3 #{:eqrr :seti}
+   12 #{:bori :addi :borr :mulr}
+   2 #{:bori :gtir :addi :borr :addr :seti :gtrr :setr :gtri}
+   11 #{:gtir :eqir :eqrr :seti :gtrr :eqri :gtri}
+   9
+   #{:bori :bani :gtir :addi :borr :banr :addr :seti :gtrr :mulr :setr
+     :gtri :muli}
+   5 #{:bori :borr :mulr :muli}
+   14 #{:bori :addi :borr :addr :seti :mulr}
+   10 #{:bori :eqir :addi :borr :addr :eqrr :seti :gtrr :gtri}
+   8 #{:bori :addi :borr :addr :mulr :muli}}
+
+  ; -- Which can be reduced to the following by recursively eliminating entries with one opcode
+  {0 :mulr
+   7 :bani
+   1 :eqri
+   4 :gtrr
+   15 :bori
+   13 :gtri
+   6 :borr
+   3 :eqrr
+   12 :addi
+   2  :setr
+   11 :gtir
+   9 :banr
+   5 :muli
+   14 :seti
+   10 :eqir
+   8 :addr}
+  ;---
+  )
